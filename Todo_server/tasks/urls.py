4626c29985b4ed3dafe82ade6483760fb66009ab
Anano24 <ananorobakidze24@gmail.com>
2023-09-22 16:48:04 +0400

    Add backend files

---
+++ Todo_server/tasks/urls.py
@@ -0,0 +1,10 @@
+from .views import TaskView
+from rest_framework import routers
+from django.urls import path, include
+
+router = routers.DefaultRouter()
+router.register(r'tasks', TaskView, 'task')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
---
+++ Todo_server/tasks/views.py
@@ -0,0 +1,11 @@
+from django.views import View
+from django.http import HttpResponse
+from rest_framework import viewsets
+from .serializers import TaskSerializer
+from .models import Task
+
+
+class TaskView(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    
